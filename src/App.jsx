a0cec43f35c27a7761f5fc8830cfede4fff6c30b
Om Preetham <ompreethambandi@gmail.com>
2024-04-13 05:50:36 -0500

    created static html with jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
 const App = () => {
-  return <main>App</main>
+  return (
+    <main className="wrapper">
+      <section className="advice">
+        <h1 className="advice__heading">Advice #117</h1>
+        <p className="advice__content">
+          "It is easy to sit up and take notice, what's difficult is getting up
+          and taking action."
+        </p>
+        <picture className="advice__divider-picture">
+          <source
+            srcSet="/images/pattern-divider-mobile.svg"
+            media="(max-width: 600px)"
+          />
+          <img
+            className="advice__divider"
+            src="/images/pattern-divider-desktop.svg"
+            alt="Pattern Divider"
+          />
+        </picture>
+        <div className="advice__dice-wrapper">
+          <img
+            className="advice__dice"
+            src="/images/icon-dice.svg"
+            alt="Dice Icon"
+          />
+        </div>
+      </section>
+    </main>
+  )
 }
 export default App
